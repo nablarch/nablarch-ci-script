f3af4ce1218dbb792749e1e1ccd2c26283cf7cc2
tamutamu <tamu.0.0.tamu@gmail.com>
2017-09-13 16:58:23 +0900

    Add nablarch-profiles-mvn-deploy.sh.

---
+++ travis-ci/nablarch-profiles-mvn-deploy.sh
@@ -0,0 +1,41 @@
+#!/bin/bash 
+set -euo pipefail
+
+
+### Import nablarch env.
+CUR=$(cd $(dirname $0); pwd)
+source ${CUR}/config/nablarch_env
+
+
+### Setting locale.
+export LC_ALL=ja_JP.UTF-8
+
+
+### Main Build.
+MVN_PROFILE=""
+while getopts p: OPT
+do
+  case $OPT in
+    "p" ) MVN_PROFILE="-P $OPTARG";;
+  esac
+done
+
+
+mvn_deploy() {
+
+  pushd $1
+
+  # Purge local repository of 'com.nablarch'.
+  mvn -s ../travis-settings.xml ${MVN_PROFILE} \
+    dependency:purge-local-repository -Dinclude=com.nablarch.* -DreResolve=false -Dverbose=true
+
+  # Deploy
+  mvn -s ../travis-settings.xml ${MVN_PROFILE} \
+    -Ddevelop_repo_url="dav:${DEVELOP_REPO_URL}/${DEVELOP_REPO_NAME}" \
+    deploy
+
+  popd
+}
+
+mvn_deploy nablarch-bom
+mvn_deploy nablarch-profile-parent
