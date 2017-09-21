acf8b59aa5d8f4b1f017ffd42d17e6a2aa0ff570
tamutamu <tamu.0.0.tamu@gmail.com>
2017-09-21 14:11:42 +0900

    Add nablarch-mvn-deploy-archetype.sh, for nablarch-single-module-archetype.

---
+++ travis-ci/nablarch-mvn-deploy-archetype.sh
@@ -0,0 +1,26 @@
+#!/bin/bash 
+set -euo pipefail
+
+
+### Import nablarch env.
+CUR=$(cd $(dirname $0); pwd)
+source ${CUR}/config/nablarch_env
+
+
+### Setting locale.
+export LC_ALL=ja_JP.UTF-8
+
+
+### Main Build.
+pushd nablarch-archetype-parent
+
+mvn -s ../travis-settings.xml \
+  -Durl=dav:${DEVELOP_REPO_URL}/${DEVELOP_REPO_NAME} \
+  -DrepositoryId=travis \
+  -Dfile=pom.xml \
+  -DpomFile=pom.xml \
+  -DgeneratePom=false \
+  -Dpackaging=pom \
+  deploy:deploy-file
+
+popd
